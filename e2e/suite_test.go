055d2f8f6866164d69804b8c0eda05fcc4d0ae1f
Yuma Ogami <yuma-ogami@cybozu.co.jp>
2024-03-01 01:52:00 +0000

    debug

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -103,9 +103,9 @@ var _ = BeforeSuite(func() {
 			fmt.Printf(string(stdout2))
 			stdout2, _, err = kubectl("get", "pv", "-oyaml")
 			fmt.Printf(string(stdout2))
-			stdout2, _, err = kubectl("get", "pvc", "-oyaml")
+			stdout2, _, err = kubectl("-n", operatorNamespace, "get", "pvc", "-oyaml")
 			fmt.Printf(string(stdout2))
-			stdout2, _, err = kubectl("describe", "pvc")
+			stdout2, _, err = kubectl("-n", operatorNamespace, "describe", "pvc")
 			fmt.Printf(string(stdout2))
 			return fmt.Errorf("kubectl get deploy failed. stderr: %s, err: %w", string(stderr), err)
 		}
