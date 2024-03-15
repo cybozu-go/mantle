df0500400bab162e834636dd9a9cf8118d62fb3f
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2024-03-15 01:55:31 +0000

    delete unused webhook
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -80,12 +80,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "RBDPVCBackup")
 		os.Exit(1)
 	}
-	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err = (&backupv1.RBDPVCBackup{}).SetupWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "RBDPVCBackup")
-			os.Exit(1)
-		}
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
