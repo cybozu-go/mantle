eb19a99355928a96de699c33cd8dddc95f1d1b79
Yuji Ito <llamerada.jp@gmail.com>
2024-05-29 07:06:36 +0000

    add MantleRestore resource
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -101,6 +101,13 @@ func subMain() error {
 		setupLog.Error(err, "unable to create controller", "controller", "MantleBackup")
 		return err
 	}
+	if err = (&controller.MantleRestoreReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MantleRestore")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
