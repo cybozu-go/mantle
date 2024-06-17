0acb8c8a80f311829fe3cba278e5569d92cd72c0
Shinya Hayashi <shinya-hayashi@cybozu.co.jp>
2024-06-17 04:32:49 +0000

    kvm driver and inter-cluster connection PoC
    Signed-off-by: Shinya Hayashi <shinya-hayashi@cybozu.co.jp>

--- versions.mk
+++ versions.mk
@@ -23,11 +23,11 @@ HELM_VERSION := 3.14.0
 # https://github.com/kubernetes-sigs/kind/releases
 KIND_VERSION := v0.20.0
 # It is set by CI using the environment variable, use conditional assignment.
-KUBERNETES_VERSION := 1.27.10
+KUBERNETES_VERSION := 1.28.3
 # https://github.com/kubernetes-sigs/kustomize/releases
 KUSTOMIZE_VERSION := v5.2.1
 # https://github.com/kubernetes/minikube/releases
-MINIKUBE_VERSION := v1.32.0
+MINIKUBE_VERSION := v1.33.1
 # https://github.com/protocolbuffers/protobuf/releases
 PROTOC_VERSION := 25.2
 # https://github.com/rook/rook/releases
