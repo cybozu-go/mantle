a590764445323985407ca58f0622af4a27ba5ad7
Ryotaro Banno <ryotaro.banno@gmail.com>
2024-11-13 07:37:55 +0000

    fix job-upload.sh to work correctly
    Signed-off-by: Ryotaro Banno <ryotaro.banno@gmail.com>

--- internal/controller/script/job-upload.sh
+++ internal/controller/script/job-upload.sh
@@ -6,4 +6,4 @@ if [ "${CERT_FILE}" = "" ]; then
   s5cmd --endpoint-url ${OBJECT_STORAGE_ENDPOINT} cp /mantle/export.bin "s3://${BUCKET_NAME}/${OBJ_NAME}"
 else
   s5cmd --endpoint-url ${OBJECT_STORAGE_ENDPOINT} --credentials-file ${CERT_FILE} cp /mantle/export.bin "s3://${BUCKET_NAME}/${OBJ_NAME}"
-end
+fi
