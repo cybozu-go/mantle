73f9968d1a916ca21ec7ea6b6892387c22751fcc
Ryotaro Banno <ryotaro.banno@gmail.com>
2024-12-25 02:42:20 +0000

    test/e2e: multik8s: correct incremental backup
    Signed-off-by: Ryotaro Banno <ryotaro.banno@gmail.com>

--- test/e2e/multik8s/suite_test.go
+++ test/e2e/multik8s/suite_test.go
@@ -355,6 +355,37 @@ func replicationTestSuite() {
 			// Make sure M0' can be used for restoration.
 			ensureCorrectRestoration(secondaryK8sCluster, ctx, namespace, backupName0, restoreName0, writtenDataHash0)
 		})
+
+		It("should perform a correct incremental backup", func(ctx SpecContext) {
+			namespace := util.GetUniqueName("ns-")
+			pvcName := util.GetUniqueName("pvc-")
+			backupName0 := util.GetUniqueName("mb-")
+			backupName1 := util.GetUniqueName("mb-")
+			restoreName0 := util.GetUniqueName("mr-")
+			restoreName1 := util.GetUniqueName("mr-")
+
+			setupEnvironment(namespace, pvcName)
+
+			// create M0.
+			writtenDataHash0 := writeRandomDataToPV(ctx, namespace, pvcName)
+			createMantleBackup(namespace, pvcName, backupName0)
+			waitMantleBackupSynced(namespace, backupName0)
+			ensureTemporaryResourcesRemoved(ctx)
+
+			// create M1.
+			writtenDataHash1 := writeRandomDataToPV(ctx, namespace, pvcName)
+			createMantleBackup(namespace, pvcName, backupName1)
+			waitMantleBackupSynced(namespace, backupName1)
+			ensureTemporaryResourcesRemoved(ctx)
+
+			// Make sure M1 and M1' have the same contents.
+			ensureCorrectRestoration(primaryK8sCluster, ctx, namespace, backupName1, restoreName1, writtenDataHash1)
+			ensureCorrectRestoration(secondaryK8sCluster, ctx, namespace, backupName1, restoreName1, writtenDataHash1)
+
+			// Make sure M0 and M0' have the same contents.
+			ensureCorrectRestoration(primaryK8sCluster, ctx, namespace, backupName0, restoreName0, writtenDataHash0)
+			ensureCorrectRestoration(secondaryK8sCluster, ctx, namespace, backupName0, restoreName0, writtenDataHash0)
+		})
 	})
 }
 
