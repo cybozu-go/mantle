c0e88ea718e718e2ab697a87ac142cabfbf05e76
Ryotaro Banno <ryotaro.banno@gmail.com>
2025-01-14 04:44:48 +0000

    test/e2e: multik8s: test removal of MB in secondary during incremental backup
    Signed-off-by: Ryotaro Banno <ryotaro.banno@gmail.com>

--- test/e2e/multik8s/replicationfailure/suite_test.go
+++ test/e2e/multik8s/replicationfailure/suite_test.go
@@ -176,5 +176,65 @@ func replicationFailureTestSuite() {
 				WaitTemporaryPVCsDeleted(ctx, primaryMB, secondaryMB)
 				WaitTemporarySecondaryPVsDeleted(ctx, secondaryMB)
 			})
+
+		It("should handle removal of MantleBackup in secondary k8s cluster during an incremental backup",
+			func(ctx SpecContext) {
+				namespace := util.GetUniqueName("ns-")
+				pvcName := util.GetUniqueName("pvc-")
+				backupName0 := util.GetUniqueName("mb-")
+				backupName1 := util.GetUniqueName("mb-")
+				restoreName0 := util.GetUniqueName("mr-")
+				restoreName1 := util.GetUniqueName("mr-")
+
+				SetupEnvironment(namespace)
+
+				// Create MantleBackup M0.
+				CreatePVC(ctx, PrimaryK8sCluster, namespace, pvcName)
+				writtenDataHash0 := WriteRandomDataToPV(ctx, PrimaryK8sCluster, namespace, pvcName)
+				CreateMantleBackup(PrimaryK8sCluster, namespace, pvcName, backupName0)
+				WaitMantleBackupSynced(namespace, backupName0)
+
+				// Pause the object storage to make backups fail.
+				PauseObjectStorage(ctx)
+				defer ResumeObjectStorage(ctx)
+
+				// Create MantleBackup M1.
+				CreatePVC(ctx, PrimaryK8sCluster, namespace, pvcName)
+				writtenDataHash1 := WriteRandomDataToPV(ctx, PrimaryK8sCluster, namespace, pvcName)
+				CreateMantleBackup(PrimaryK8sCluster, namespace, pvcName, backupName1)
+
+				// Wait until an upload Job is created.
+				WaitUploadJobCreated(ctx, PrimaryK8sCluster, namespace, backupName1)
+
+				primaryMB1, err := GetMB(PrimaryK8sCluster, namespace, backupName1)
+				Expect(err).NotTo(HaveOccurred())
+				secondaryMB10, err := GetMB(SecondaryK8sCluster, namespace, backupName1)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Delete MantleBackup M1'.
+				DeleteMantleBackup(SecondaryK8sCluster, namespace, backupName1)
+
+				// Resume the object storage so that M1' will be deleted correctly after its finalization.
+				ResumeObjectStorage(ctx)
+
+				// M1' should be re-created and synced.
+				WaitMantleBackupSynced(namespace, backupName1)
+
+				// Make sure the PVC in the primary and secondary cluster DOES have a snapshot.
+				EnsurePVCHasSnapshot(PrimaryK8sCluster, namespace, pvcName, backupName1)
+				EnsurePVCHasSnapshot(SecondaryK8sCluster, namespace, pvcName, backupName1)
+
+				// Make sure we can restore correct data from M0, M0', M1, and M1'.
+				EnsureCorrectRestoration(PrimaryK8sCluster, ctx, namespace, backupName0, restoreName0, writtenDataHash0)
+				EnsureCorrectRestoration(SecondaryK8sCluster, ctx, namespace, backupName0, restoreName0, writtenDataHash0)
+				EnsureCorrectRestoration(PrimaryK8sCluster, ctx, namespace, backupName1, restoreName1, writtenDataHash1)
+				EnsureCorrectRestoration(SecondaryK8sCluster, ctx, namespace, backupName1, restoreName1, writtenDataHash1)
+
+				// Make sure all unnecessary resources are removed.
+				secondaryMB11, err := GetMB(SecondaryK8sCluster, namespace, backupName1)
+				Expect(err).NotTo(HaveOccurred())
+				WaitTemporaryResourcesDeleted(ctx, primaryMB1, secondaryMB10)
+				WaitTemporaryResourcesDeleted(ctx, primaryMB1, secondaryMB11)
+			})
 	})
 }
