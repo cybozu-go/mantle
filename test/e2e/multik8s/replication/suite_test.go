7d2739a6a4d2628db83a83e749b1a6fea5475d08
Ryotaro Banno <ryotaro.banno@gmail.com>
2025-02-19 07:51:19 +0000

    test/e2e: multik8s: attach various-transfer-part-size label
    Signed-off-by: Ryotaro Banno <ryotaro.banno@gmail.com>

--- test/e2e/multik8s/replication/suite_test.go
+++ test/e2e/multik8s/replication/suite_test.go
@@ -41,165 +41,171 @@ var _ = Describe("Mantle", func() {
 
 func replicationTestSuite() { //nolint:gocyclo
 	Describe("replication test", func() {
-		It("should correctly replicate PVC and MantleBackup resources", func(ctx SpecContext) {
-			namespace := util.GetUniqueName("ns-")
-			pvcName := util.GetUniqueName("pvc-")
-			backupName := util.GetUniqueName("mb-")
-			restoreName := util.GetUniqueName("mr-")
+		It("should correctly replicate PVC and MantleBackup resources",
+			// This test case is intended to be run with various backup transfer
+			// part size. This label is attached mainly for CI. See also
+			// e2e-multiple-k8s-clusters.yaml workflow.
+			Label("various-transfer-part-size"),
+			func(ctx SpecContext) {
+				namespace := util.GetUniqueName("ns-")
+				pvcName := util.GetUniqueName("pvc-")
+				backupName := util.GetUniqueName("mb-")
+				restoreName := util.GetUniqueName("mr-")
 
-			SetupEnvironment(namespace)
-			CreatePVC(ctx, PrimaryK8sCluster, namespace, pvcName)
-			writtenDataHash := WriteRandomDataToPV(ctx, PrimaryK8sCluster, namespace, pvcName)
-			CreateMantleBackup(PrimaryK8sCluster, namespace, pvcName, backupName)
-			WaitMantleBackupSynced(namespace, backupName)
+				SetupEnvironment(namespace)
+				CreatePVC(ctx, PrimaryK8sCluster, namespace, pvcName)
+				writtenDataHash := WriteRandomDataToPV(ctx, PrimaryK8sCluster, namespace, pvcName)
+				CreateMantleBackup(PrimaryK8sCluster, namespace, pvcName, backupName)
+				WaitMantleBackupSynced(namespace, backupName)
 
-			By("checking PVC is replicated")
-			Eventually(func() error {
-				primaryPVC, err := GetPVC(PrimaryK8sCluster, namespace, pvcName)
-				if err != nil {
-					return err
-				}
-
-				pvc, err := GetPVC(SecondaryK8sCluster, namespace, pvcName)
-				if err != nil {
-					return err
-				}
-				if pvc.Annotations == nil ||
-					pvc.Annotations["mantle.cybozu.io/remote-uid"] != string(primaryPVC.GetUID()) {
-					return errors.New("invalid remote-uid annotation")
-				}
-				primaryPVC.Spec.VolumeName = ""
-				pvc.Spec.VolumeName = ""
-				if !reflect.DeepEqual(primaryPVC.Spec, pvc.Spec) {
-					return errors.New("spec not equal")
-				}
-				if pvc.Status.Phase != corev1.ClaimBound {
-					return errors.New("pvc not bound")
-				}
-
-				return nil
-			}).Should(Succeed())
-
-			By("checking MantleBackup is replicated")
-			var primaryMB, secondaryMB *mantlev1.MantleBackup
-			Eventually(func() error {
-				primaryPVC, err := GetPVC(PrimaryK8sCluster, namespace, pvcName)
-				if err != nil {
-					return err
-				}
-				secondaryPVC, err := GetPVC(SecondaryK8sCluster, namespace, pvcName)
-				if err != nil {
-					return err
-				}
-				primaryMB, err = GetMB(PrimaryK8sCluster, namespace, backupName)
-				if err != nil {
-					return err
-				}
-
-				secondaryMB, err = GetMB(SecondaryK8sCluster, namespace, backupName)
-				if err != nil {
-					return err
-				}
-				if !controllerutil.ContainsFinalizer(secondaryMB, "mantlebackup.mantle.cybozu.io/finalizer") {
-					return errors.New("finalizer not found")
-				}
-				if secondaryMB.Labels == nil ||
-					secondaryMB.Labels["mantle.cybozu.io/local-backup-target-pvc-uid"] != string(secondaryPVC.GetUID()) ||
-					secondaryMB.Labels["mantle.cybozu.io/remote-backup-target-pvc-uid"] != string(primaryPVC.GetUID()) {
-					return errors.New("local/remote-backup-target-pvc-uid label not matched")
-				}
-				if secondaryMB.Annotations == nil ||
-					secondaryMB.Annotations["mantle.cybozu.io/remote-uid"] != string(primaryMB.GetUID()) {
-					return errors.New("remote-uid not matched")
-				}
-				if !reflect.DeepEqual(primaryMB.Spec, secondaryMB.Spec) {
-					return errors.New("spec not equal")
-				}
-				if secondaryMB.Status.CreatedAt.IsZero() {
-					return errors.New(".Status.CreatedAt is zero")
-				}
-				if !meta.IsStatusConditionTrue(secondaryMB.Status.Conditions, "ReadyToUse") {
-					return errors.New("ReadyToUse of .Status.Conditions is not True")
-				}
-
-				// Check if snapshots are created correctly in the secondary Rook/Ceph cluster
-				snap, err := FindRBDSnapshotInPVC(SecondaryK8sCluster,
-					secondaryPVC.GetNamespace(), secondaryPVC.GetName(), secondaryMB.GetName())
-				if err != nil {
-					return fmt.Errorf("failed to find snapshot in PVC: %w", err)
-				}
-				if secondaryMB.Status.SnapID == nil || snap.Id != *secondaryMB.Status.SnapID {
-					return errors.New("invalid .status.snapID of secondary MB")
-				}
-
-				// Check if .status.LargestCompleted{Export,Import,Upload}PartNum are correct
-				backupTransferPartSize, err := GetBackupTransferPartSize()
-				if err != nil {
-					return fmt.Errorf("failed to get backup transfer part size :%w", err)
-				}
-				pvcSize := primaryPVC.Spec.Resources.Requests.Storage()
-				expectedNumOfParts := 1
-				if pvcSize.Cmp(*backupTransferPartSize) > 0 { // pvcSize > backupTransferPartSize
-					backupTransferPartSize, ok := backupTransferPartSize.AsInt64()
-					if !ok {
-						return errors.New("failed to convert backup transfer part size to i64")
+				By("checking PVC is replicated")
+				Eventually(func() error {
+					primaryPVC, err := GetPVC(PrimaryK8sCluster, namespace, pvcName)
+					if err != nil {
+						return err
 					}
-					pvcSizeI64, ok := pvcSize.AsInt64()
-					if !ok {
-						return errors.New("failed to convert pvc size to i64")
+
+					pvc, err := GetPVC(SecondaryK8sCluster, namespace, pvcName)
+					if err != nil {
+						return err
 					}
-					expectedNumOfParts = int(pvcSizeI64 / backupTransferPartSize)
-					if pvcSizeI64%backupTransferPartSize != 0 {
-						expectedNumOfParts++
+					if pvc.Annotations == nil ||
+						pvc.Annotations["mantle.cybozu.io/remote-uid"] != string(primaryPVC.GetUID()) {
+						return errors.New("invalid remote-uid annotation")
 					}
-				}
+					primaryPVC.Spec.VolumeName = ""
+					pvc.Spec.VolumeName = ""
+					if !reflect.DeepEqual(primaryPVC.Spec, pvc.Spec) {
+						return errors.New("spec not equal")
+					}
+					if pvc.Status.Phase != corev1.ClaimBound {
+						return errors.New("pvc not bound")
+					}
 
-				if primaryMB.Status.LargestCompletedExportPartNum == nil {
-					return fmt.Errorf(
-						".status.largestCompletedExportPartNum of the primary MB is not correct: expected %d: got nil",
-						expectedNumOfParts,
-					)
-				} else if *primaryMB.Status.LargestCompletedExportPartNum != expectedNumOfParts-1 {
-					return fmt.Errorf(
-						".status.largestCompletedExportPartNum of the primary MB is not correct: expected %d: got %d",
-						expectedNumOfParts,
-						*primaryMB.Status.LargestCompletedExportPartNum,
-					)
-				}
+					return nil
+				}).Should(Succeed())
 
-				if primaryMB.Status.LargestCompletedUploadPartNum == nil {
-					return fmt.Errorf(
-						".status.largestCompletedUploadPartNum of the primary MB is not correct: expected %d: got nil",
-						expectedNumOfParts,
-					)
-				} else if *primaryMB.Status.LargestCompletedUploadPartNum != expectedNumOfParts-1 {
-					return fmt.Errorf(
-						".status.largestCompletedUploadPartNum of the primary MB is not correct: expected %d: got %d",
-						expectedNumOfParts,
-						*primaryMB.Status.LargestCompletedUploadPartNum,
-					)
-				}
+				By("checking MantleBackup is replicated")
+				var primaryMB, secondaryMB *mantlev1.MantleBackup
+				Eventually(func() error {
+					primaryPVC, err := GetPVC(PrimaryK8sCluster, namespace, pvcName)
+					if err != nil {
+						return err
+					}
+					secondaryPVC, err := GetPVC(SecondaryK8sCluster, namespace, pvcName)
+					if err != nil {
+						return err
+					}
+					primaryMB, err = GetMB(PrimaryK8sCluster, namespace, backupName)
+					if err != nil {
+						return err
+					}
 
-				if secondaryMB.Status.LargestCompletedImportPartNum == nil {
-					return fmt.Errorf(
-						".status.largestCompletedImportPartNum of the secondary MB is not correct: expected %d: got nil",
-						expectedNumOfParts,
-					)
-				} else if *secondaryMB.Status.LargestCompletedImportPartNum != expectedNumOfParts-1 {
-					return fmt.Errorf(
-						".status.largestCompletedImportPartNum of the secondary MB is not correct: expected %d: got %d",
-						expectedNumOfParts,
-						*secondaryMB.Status.LargestCompletedImportPartNum,
-					)
-				}
+					secondaryMB, err = GetMB(SecondaryK8sCluster, namespace, backupName)
+					if err != nil {
+						return err
+					}
+					if !controllerutil.ContainsFinalizer(secondaryMB, "mantlebackup.mantle.cybozu.io/finalizer") {
+						return errors.New("finalizer not found")
+					}
+					if secondaryMB.Labels == nil ||
+						secondaryMB.Labels["mantle.cybozu.io/local-backup-target-pvc-uid"] != string(secondaryPVC.GetUID()) ||
+						secondaryMB.Labels["mantle.cybozu.io/remote-backup-target-pvc-uid"] != string(primaryPVC.GetUID()) {
+						return errors.New("local/remote-backup-target-pvc-uid label not matched")
+					}
+					if secondaryMB.Annotations == nil ||
+						secondaryMB.Annotations["mantle.cybozu.io/remote-uid"] != string(primaryMB.GetUID()) {
+						return errors.New("remote-uid not matched")
+					}
+					if !reflect.DeepEqual(primaryMB.Spec, secondaryMB.Spec) {
+						return errors.New("spec not equal")
+					}
+					if secondaryMB.Status.CreatedAt.IsZero() {
+						return errors.New(".Status.CreatedAt is zero")
+					}
+					if !meta.IsStatusConditionTrue(secondaryMB.Status.Conditions, "ReadyToUse") {
+						return errors.New("ReadyToUse of .Status.Conditions is not True")
+					}
 
-				return nil
-			}).Should(Succeed())
+					// Check if snapshots are created correctly in the secondary Rook/Ceph cluster
+					snap, err := FindRBDSnapshotInPVC(SecondaryK8sCluster,
+						secondaryPVC.GetNamespace(), secondaryPVC.GetName(), secondaryMB.GetName())
+					if err != nil {
+						return fmt.Errorf("failed to find snapshot in PVC: %w", err)
+					}
+					if secondaryMB.Status.SnapID == nil || snap.Id != *secondaryMB.Status.SnapID {
+						return errors.New("invalid .status.snapID of secondary MB")
+					}
 
-			WaitTemporaryResourcesDeleted(ctx, primaryMB, secondaryMB)
-			EnsureCorrectRestoration(PrimaryK8sCluster, ctx, namespace, backupName, restoreName, writtenDataHash)
-			EnsureCorrectRestoration(SecondaryK8sCluster, ctx, namespace, backupName, restoreName, writtenDataHash)
-		})
+					// Check if .status.LargestCompleted{Export,Import,Upload}PartNum are correct
+					backupTransferPartSize, err := GetBackupTransferPartSize()
+					if err != nil {
+						return fmt.Errorf("failed to get backup transfer part size :%w", err)
+					}
+					pvcSize := primaryPVC.Spec.Resources.Requests.Storage()
+					expectedNumOfParts := 1
+					if pvcSize.Cmp(*backupTransferPartSize) > 0 { // pvcSize > backupTransferPartSize
+						backupTransferPartSize, ok := backupTransferPartSize.AsInt64()
+						if !ok {
+							return errors.New("failed to convert backup transfer part size to i64")
+						}
+						pvcSizeI64, ok := pvcSize.AsInt64()
+						if !ok {
+							return errors.New("failed to convert pvc size to i64")
+						}
+						expectedNumOfParts = int(pvcSizeI64 / backupTransferPartSize)
+						if pvcSizeI64%backupTransferPartSize != 0 {
+							expectedNumOfParts++
+						}
+					}
+
+					if primaryMB.Status.LargestCompletedExportPartNum == nil {
+						return fmt.Errorf(
+							".status.largestCompletedExportPartNum of the primary MB is not correct: expected %d: got nil",
+							expectedNumOfParts,
+						)
+					} else if *primaryMB.Status.LargestCompletedExportPartNum != expectedNumOfParts-1 {
+						return fmt.Errorf(
+							".status.largestCompletedExportPartNum of the primary MB is not correct: expected %d: got %d",
+							expectedNumOfParts,
+							*primaryMB.Status.LargestCompletedExportPartNum,
+						)
+					}
+
+					if primaryMB.Status.LargestCompletedUploadPartNum == nil {
+						return fmt.Errorf(
+							".status.largestCompletedUploadPartNum of the primary MB is not correct: expected %d: got nil",
+							expectedNumOfParts,
+						)
+					} else if *primaryMB.Status.LargestCompletedUploadPartNum != expectedNumOfParts-1 {
+						return fmt.Errorf(
+							".status.largestCompletedUploadPartNum of the primary MB is not correct: expected %d: got %d",
+							expectedNumOfParts,
+							*primaryMB.Status.LargestCompletedUploadPartNum,
+						)
+					}
+
+					if secondaryMB.Status.LargestCompletedImportPartNum == nil {
+						return fmt.Errorf(
+							".status.largestCompletedImportPartNum of the secondary MB is not correct: expected %d: got nil",
+							expectedNumOfParts,
+						)
+					} else if *secondaryMB.Status.LargestCompletedImportPartNum != expectedNumOfParts-1 {
+						return fmt.Errorf(
+							".status.largestCompletedImportPartNum of the secondary MB is not correct: expected %d: got %d",
+							expectedNumOfParts,
+							*secondaryMB.Status.LargestCompletedImportPartNum,
+						)
+					}
+
+					return nil
+				}).Should(Succeed())
+
+				WaitTemporaryResourcesDeleted(ctx, primaryMB, secondaryMB)
+				EnsureCorrectRestoration(PrimaryK8sCluster, ctx, namespace, backupName, restoreName, writtenDataHash)
+				EnsureCorrectRestoration(SecondaryK8sCluster, ctx, namespace, backupName, restoreName, writtenDataHash)
+			},
+		)
 
 		//nolint:dupl
 		It("should back up correctly if previous MB is deleted in the secondary cluster", func(ctx SpecContext) {
