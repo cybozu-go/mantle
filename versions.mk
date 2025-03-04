7b87e9e1254f245af544feb15c2edb622481e0b6
Ryotaro Banno <ryotaro.banno@gmail.com>
2025-03-04 09:32:20 +0000

    bump tool versions

--- versions.mk
+++ versions.mk
@@ -1,21 +1,21 @@
 # https://github.com/kubernetes-sigs/controller-tools/releases
 CONTROLLER_TOOLS_VERSION := v0.16.4
 # https://github.com/helm/helm/releases
-HELM_VERSION := 3.16.2
+HELM_VERSION := 3.17.1
 # It is set by CI using the environment variable, use conditional assignment.
 KUBERNETES_VERSION := 1.31.1
 # https://github.com/kubernetes-sigs/kustomize/releases
-KUSTOMIZE_VERSION := v5.5.0
+KUSTOMIZE_VERSION := v5.6.0
 # https://github.com/kubernetes/minikube/releases
-MINIKUBE_VERSION := v1.34.0
+MINIKUBE_VERSION := v1.35.0
 # https://github.com/uber-go/mock/releases
 MOCKGEN_VERSION := v0.5.0
 # https://github.com/protocolbuffers/protobuf/releases
-PROTOC_VERSION := 28.3
+PROTOC_VERSION := 29.3
 # https://github.com/rook/rook/releases
-ROOK_CHART_VERSION := v1.15.4
+ROOK_CHART_VERSION := v1.16.4
 # https://github.com/golangci/golangci-lint/releases
-GOLANGCI_LINT_VERSION := v1.61.0
+GOLANGCI_LINT_VERSION := v1.64.5
 
 # Tools versions which are defined in go.mod
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
