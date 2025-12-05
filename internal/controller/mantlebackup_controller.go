31377496bc028ff699b786eeed91a87f61a5c939
Ryotaro Banno <ryotaro.banno@gmail.com>
2025-12-05 02:36:07 +0000

    chore: controller: fix error messages

--- internal/controller/mantlebackup_controller.go
+++ internal/controller/mantlebackup_controller.go
@@ -1109,7 +1109,7 @@ func (r *MantleBackupReconciler) startExportAndUpload(
 			DiffFrom:  sourceBackupName,
 		},
 	); err != nil {
-		return ctrl.Result{}, err
+		return ctrl.Result{}, fmt.Errorf("failed to call SetSynchronizing RPC: %w", err)
 	}
 
 	largestCompletedExportPartNum, err := r.startExport(ctx, targetBackup, sourceBackupName)
@@ -2470,7 +2470,7 @@ func (r *MantleBackupReconciler) reconcileImportJob(
 	// Check that all import Jobs are completed
 	finalPartNum, err := r.getNumberOfParts(backup)
 	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to calcuate num of export data parts: %w", err)
+		return ctrl.Result{}, fmt.Errorf("failed to calculate num of export data parts: %w", err)
 	}
 	if partNum == finalPartNum {
 		// Make sure the (final) RBD snapshot is created.
